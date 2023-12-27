5069a623e5e769cee79332d9c83d465184c1c059
naoxin20040419 <13369459917@163.com>
2023-12-27 12:09:07 +0800

    zzz

---
+++ packages/frontend/dist/assets/CommunityView-aHgJ91ps.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-stNcuBHF.js";const e={name:"CommunityView"};function n(o,r,s,c,_,a){return null}const m=t(e,[["render",n]]);export{m as default};
--- packages/frontend/src/router/index.js
+++ packages/frontend/src/router/index.js
@@ -6,129 +6,136 @@ import { showMsg } from 'common'
 import { msg } from 'common'
 const { token } = useToken()
 
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: {
-        showNav: true
-      }
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      showNav: true
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      showNav: true
+    }
+  },
+  {
+    path: '/community',
+    name: 'community',
+    component: () => import('../views/CommunityView.vue'),
+    meta: {
+      showNav: true
+    }
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: () => import('../views/HistoryView.vue'),
+    meta: {
+      showNav: true
+    }
+  },
+  {
+    path: '/favorite',
+    name: 'favorite',
+    component: () => import('../views/FavoriteView.vue'),
+    meta: {
+      showNav: true
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta: {
-        showNav: true
-      }
+    beforeEnter: (to, from, next) => {
+      token.value
+        ? next()
+        : showMsg({
+          msg: msg['NO_TOKEN'],
+          messageType: 'info',
+          popupType: 'alert'
+        })
+    }
+  },
+  {
+    path: '/book/:id',
+    name: 'book',
+    component: () => import('../views/BookView.vue'),
+    meta: {
+      showNav: false
     },
-    {
-      path: '/community',
-      name: 'community',
-      component: () => import('../views/CommunityView.vue'),
-      meta: {
-        showNav: true
-      }
+    beforeEnter: (to, from, next) => {
+      storeHistory(to.params.id)
+      next()
+    }
+  },
+  {
+    path: '/read/:id/:chapter',
+    name: 'read',
+    component: () => import('../views/ReadView.vue'),
+    meta: {
+      showNav: false
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login-RegisterView.vue'),
+    meta: {
+      showNav: false
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Login-RegisterView.vue'),
+    meta: {
+      showNav: false
+    }
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue'),
+    meta: {
+      showNav: true
     },
-    {
-      path: '/history',
-      name: 'history',
-      component: () => import('../views/HistoryView.vue'),
-      meta: {
-        showNav: true
-      }
-    },
-    {
-      path: '/favorite',
-      name: 'favorite',
-      component: () => import('../views/FavoriteView.vue'),
-      meta: {
-        showNav: true
-      },
-      beforeEnter: (to, from, next) => {
-        token.value
-          ? next()
-          : showMsg({
-              msg: msg['NO_TOKEN'],
-              messageType: 'info',
-              popupType: 'alert'
-            })
-      }
-    },
-    {
-      path: '/book/:id',
-      name: 'book',
-      component: () => import('../views/BookView.vue'),
-      meta: {
-        showNav: false
-      },
-      beforeEnter: (to, from, next) => {
-        storeHistory(to.params.id)
-        next()
-      }
-    },
-    {
-      path: '/read/:id/:chapter',
-      name: 'read',
-      component: () => import('../views/ReadView.vue'),
-      meta: {
-        showNav: false
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/Login-RegisterView.vue'),
-      meta: {
-        showNav: false
-      }
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/Login-RegisterView.vue'),
-      meta: {
-        showNav: false
-      }
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: () => import('../views/ProfileView.vue'),
-      meta: {
-        showNav: true
-      },
-      beforeEnter: (to, from, next) => {
-        token.value
-          ? next()
-          : showMsg({
-              msg: msg['NO_TOKEN'],
-              messageType: 'info',
-              popupType: 'toast'
-            })
-      }
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: () => import('../views/SearchView.vue'),
-      meta: {
-        showNav: false
-      }
-    },
+    beforeEnter: (to, from, next) => {
+      token.value
+        ? next()
+        : showMsg({
+          msg: msg['NO_TOKEN'],
+          messageType: 'info',
+          popupType: 'toast'
+        })
+    }
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/SearchView.vue'),
+    meta: {
+      showNav: false
+    }
+  },
+]
+
+if (import.meta.env.MODE === 'development') {
+  routes.push(
     {
       path: '/test',
       name: 'test',
       component: () => import('../views/TestView.vue')
     }
-  ],
-  scrollBehavior(to, from, savedPosition) {
+  )
+}
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to) {
     if (to.name === 'read') {
       const { id, chapter } = to.params
       const ele = localStorage.getItem(`${id}-${chapter}`)
